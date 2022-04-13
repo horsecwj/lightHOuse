bd6e5cf66c143fca99f3f5542472b40b3c2ac71d
wly <1228022817@qq.com>
2022-04-13 10:11:37 +0800

    修复：去除覆盖错误日志的干扰打印

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker build --rm -t 831273654673.dkr.ecr.ap-southeast-1.amazonaws.com/light-hou
 #docker push 831273654673.dkr.ecr.ap-southeast-1.amazonaws.com/light-house/rear-end:latest
 docker save 831273654673.dkr.ecr.ap-southeast-1.amazonaws.com/light-house/rear-end:latest -o rear-end.zip
 scp rear-end.zip root@47.57.137.142:/root/
-#ssh root@47.57.137.142
+ssh root@47.57.137.142 "docker load -i /root/rear-end.zip && docker rm -f lighthouse && bash /root/light-house/run_lighthouse.sh"
